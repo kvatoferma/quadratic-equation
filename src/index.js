430c7a6fe96daf05a50a337ccb875105779d5fa5
kvatoferma <jimmaggot@mail.ru>
2018-09-17 11:46:00 +0300

    100 percent pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let coeffs =  equation.replace(/\s/g, '')
+                              .split(/\*x\^2|\*x/)
+                              .map(item => parseInt(item));
+  let a = coeffs[0];
+  let b = coeffs[1];
+  let c = coeffs[2];
+  let D = b**2 - 4*a*c;
+  let x1 = (-1)*(b + Math.sqrt(D))/(2*a);
+  let x2 = (-1)*(b - Math.sqrt(D))/(2*a);
+  let roots = [x1, x2].map(root => Math.round(root))
+                                  .sort(comparator);
+  return roots;
+  
+  function comparator(a, b) {
+    return (a !== b) ? ((a < b) ? -1 : 1) : 0;
+  }
 }
